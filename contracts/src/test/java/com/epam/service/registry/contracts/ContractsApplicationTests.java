590ea61059b6f520af2d6108d43f1c95f5667cdd
tutkuince <tutku.ince@outlook.com>
2025-09-18 20:34:03 +0300

    contracts

---
+++ contracts/src/test/java/com/epam/service/registry/contracts/ContractsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.service.registry.contracts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
