7076302c039d7b9a3054e3b6a8de361dc558e09a
tutkuince <tutku.ince@outlook.com>
2025-09-18 21:06:45 +0300

    update

---
+++ gym-crm-contracts/src/main/java/com/epam/contracts/GymCrmContractsApplication.java
@@ -0,0 +1,13 @@
+package com.epam.contracts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymCrmContractsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymCrmContractsApplication.class, args);
+    }
+
+}
